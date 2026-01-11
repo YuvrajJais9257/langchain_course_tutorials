6bbe40ff3601fb57b46ba6315ad1d2c43afc18c0
Yuvraj Jaiswal <quicksilver92571331@gmail.com>
2026-01-12 03:01:43 +0530

    updated react_search_agent dependencies

--- react_search_agent/app.py
+++ react_search_agent/app.py
@@ -1,38 +1,48 @@
 from dotenv import load_dotenv
 load_dotenv()
 
+from langchain_core.prompts import PromptTemplate
 from langchain import hub
 from langchain.agents import AgentExecutor, create_react_agent
 from langchain_groq import ChatGroq
 from langchain_community.tools.tavily_search import TavilySearchResults
+from langchain_core.output_parsers import PydanticOutputParser
+from schemas import AgentResponse
 
-# Tool
-tools = [TavilySearchResults(max_results=3)]
+#brain
+llm = ChatGroq(model="llama-3.3-70b-versatile", temperature=0.1)
 
-# LLM
-llm = ChatGroq(model="llama-3.3-70b-versatile", temperature=0.2)
+#tools
+search_tool = TavilySearchResults(max_results=5)
 
-# Prompt from LangChain Hub
+from prompt import REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS
+
+#prompt
 react_prompt = hub.pull("hwchase17/react")
+output_parser = PydanticOutputParser(pydantic_object=AgentResponse)
+react_prompt_with_format_instructions = PromptTemplate(
+    template=REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS,
+    input_variables=["input","agent_scratchpad","tool_names"]
+).partial(format_instructions=output_parser.get_format_instructions())
 
-# Create ReAct agent
-agent = create_react_agent(
+#agent
+react_agent=create_react_agent(
     llm=llm,
-    tools=tools,
-    prompt=react_prompt,
+    tools=[search_tool],
+    prompt=react_prompt_with_format_instructions,
 )
 
-agent_executor = AgentExecutor(
-    agent=agent,
-    tools=tools,
+react_agent_executor = AgentExecutor(
+    agent=react_agent,
+    tools=[search_tool],
     verbose=True,
     handle_parsing_errors=True,
     max_iterations=5
 )
 
-# Smoke test
-result = agent_executor.invoke(
-    {"input": "Who created LangChain and what problem does it solve?"}
+#test_run
+result=react_agent_executor.invoke(
+    {"input": "Find the latest AI Engineer job openings and provide links to apply."}
 )
 
 print("\n" + "="*50)
